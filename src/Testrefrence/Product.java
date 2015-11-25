c982f2bc5098b8d7e12b90eb8316845698b0be7a
gutao <gutaoai@126.com>
2015-11-25 22:37:43 +0800

    gutao

--- src/Testrefrence/Product.java
+++ src/Testrefrence/Product.java
@@ -19,29 +19,32 @@ public class Product {
 	   return i;
    }
    public static void main(String[] args) {
-//	 Product p1 = new Product(7);
-//	 System.out.println("main:"+p1.i);
-//	 testobj(p1);
-//	 p1.i++;
-//     System.out.println(testobj(p1));
-//	   testobjdata(p1.i);
-//	   System.out.println(p1.i);
-	Product p = new Product();
-	Product p1 = new Product();
-	System.out.println(p);
-	System.out.println(p1);
-	System.out.println("-----------------");
-	p1 = p;
-	System.out.println("--p的值："+p);
-	System.out.println("--p1的值"+p1);
+	 Product p1 = new Product(7);
+	 System.out.println("main:"+p1.i);
+	 testobj(p1);
+	 p1.i++;
+     System.out.println(testobj(p1));
+	   testobjdata(p1.i);
+	   System.out.println(p1.i);
+	   
+//	Product p = new Product();
+//	Product p1 = new Product();
+//	System.out.println(p);
+//	System.out.println(p1);
+//	System.out.println("-----------------");
+//	p1 = p;
+//	System.out.println("--p的值："+p);
+//	System.out.println("--p1的值"+p1);
+//	
+//	Product p = new Product();
+//	Product p1 = new Product();
+//	p1 = p;
+//	System.out.println("--p的值："+p.i);
+//	System.out.println("--p1的值"+p1.i);
+//	System.out.println("--------更改后-------------");
+//	p.i++;
+//	System.out.println("--p的值："+p.i);
+//	System.out.println("--p1的值"+p1.i);
    }
 }
-//Product p = new Product();
-//Product p1 = new Product();
-//p1 = p;
-//System.out.println("--p的值："+p.i);
-//System.out.println("--p1的值"+p1.i);
-//System.out.println("--------更改后-------------");
-//p.i++;
-//System.out.println("--p的值："+p.i);
-//System.out.println("--p1的值"+p1.i);
+
--- src/Testrefrence/Test1.java
+++ src/Testrefrence/Test1.java
@@ -1,5 +1,11 @@
 package Testrefrence;
 
 public class Test1 {
-
+  public static void main(String[] args) {
+	  //""修饰的都属于常量，类的所有信息会被加载在方法区中，new出来的对象会放在堆中指向方法去中对应的
+	  //类信息和常量，栈中对应存放的是局部变量，方法区还存放着对应的类的代码信息和静态变量，已经常量池
+	String s1 = "gutao";
+	String s2 = "gutao";
+	System.out.println(s1==s2);
+}
 }
