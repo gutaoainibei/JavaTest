a1088e8b939a4d1e4da65b8af1b74d10b1743223
gutao <gutaoai@126.com>
2015-12-11 23:54:12 +0800

    测试String类

---
+++ src/TestString/TestString.java
@@ -0,0 +1,14 @@
+package TestString;
+/**
+ * 
+* @ClassName: TestString 
+* @Description: TODO(测试字符串类) 
+* @author gutao 
+* @date 2015年12月11日 下午11:53:02 
+*
+ */
+public class TestString {
+  public static void main(String[] args) {
+	
+}
+}
