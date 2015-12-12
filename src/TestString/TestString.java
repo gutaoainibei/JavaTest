e01f9b240c6f42461e2e6079f6cfc0954c9eae2f
gutao <gutaoai@126.com>
2015-12-12 23:11:47 +0800

    测试String完毕

--- src/TestString/TestString.java
+++ src/TestString/TestString.java
@@ -26,10 +26,23 @@ public class TestString {
 	String stringcompare1 = new String("aaa");
 	String stringcompare2 = new String("aaa");
 	System.out.println("strcompare1=strcompare2="+strcompare1.equals(strcompare2));
-	System.out.println(strcompare1==strcompare2);
-	System.out.println(stringcompare1==stringcompare2);
+	System.out.println("test1:"+(strcompare1==strcompare2));
+	System.out.println("test2:"+(stringcompare1==stringcompare2));
 	System.out.println(stringcompare1.equals(stringcompare2));
-	System.out.println(stringcompare1==strcompare2);
-
+	System.out.println("test3:"+(stringcompare1==strcompare2));
+	
+	String g = "asdf";
+	g = "qwer";
+	System.out.println(g);
+	//这里String new了四个对象，分别是"a","b","a+b"以及new的一个s1指向的String对象
+	//我们通常知道的final对象是不可以变得，确实是不可变得，但是我们发觉重新赋值好像也可以
+	//其实那是重新new了一个对象
+	//这里
+	String s1 = new String("a"+"b");
+	//和上面不一样，这里只有三个对象，"a","b","a+b"
+	String s2 = "a"+"b";
+    System.out.println(g.startsWith("q"));
+    System.out.println(g.endsWith("r"));
+    System.out.println(g.equalsIgnoreCase("Qwer"));
 }
 }
