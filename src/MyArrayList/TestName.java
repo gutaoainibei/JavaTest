b5bb12c9bc86260fa6c32fbc476d006bce3caca5
gutao <gutaoai@126.com>
2015-12-16 22:46:45 +0800

    test the link the method of toString of Arrays and Object

--- src/MyArrayList/TestName.java
+++ src/MyArrayList/TestName.java
@@ -1,9 +1,13 @@
 package MyArrayList;
 
+import java.util.Arrays;
 
 public class TestName {
-	
+	//这里说明了Arrays的toString和Object的toString的方法的关系是重载
+	public static String toString(int i){
+		return "";
+	}
 	public static void main(String[] args) {
-		
+	  
 	}
 }
