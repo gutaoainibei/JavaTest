792a3fe0d979172c03f94e905d0ae7d5da376140
gutao <gutaoai@126.com>
2015-12-22 23:43:06 +0800

    Test File Class1

--- src/TestFile/TestFile.java
+++ src/TestFile/TestFile.java
@@ -1,5 +1,23 @@
 package TestFile;
 
-public class TestFile {
+import java.io.File;
+import java.io.IOException;
 
+import com.mysql.jdbc.Field;
+
+public class TestFile {
+   public static void main(String[] args) {
+	File file1 = new File("d:/Test.java");
+	File file2 = new File("d:");
+	File file3 = new File(file2, "Test.java");
+	File file4 = new File("d:/test.txt");
+	file4.delete();
+	File file5 = new File("d:/aaa/ddd/ccc");
+	
+	file5.deleteOnExit();//从最深目录删，不能删根
+	System.out.println(file1.isFile());
+	System.out.println(file2.isDirectory());
+	System.out.println(file3.isFile());
+	
+}
 }
