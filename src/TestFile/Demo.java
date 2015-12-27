2545490ffa968e86ead0e1bc3cf844d57aaef9aa
gutao <gutaoai@126.com>
2015-12-27 22:59:33 +0800

    Test File dirc

--- src/TestFile/Demo.java
+++ src/TestFile/Demo.java
@@ -1,8 +1,22 @@
 package TestFile;
 
+import java.io.File;
+
 public class Demo {
 public static void main(String[] args) {
-	System.err.println("今天不会怎么更新");
-	System.out.println("看来今天也没办法更新了");
+	Demo demo = new Demo();
+	demo.ShowFileDirc(new File("G:/风为殇"), 0);
+}
+public void ShowFileDirc(File file , int level){
+	for(int i = 0 ;i<level ; i++){
+		System.out.print("-");
+	}
+	System.out.println(file.getName());
+	if(file.isDirectory()){
+		File[] files = file.listFiles();
+        for(File tempfile : files){
+        	ShowFileDirc(tempfile, level+1);
+        }
+	}
 }
 }
