21816b6fd166dd034528d55e2ead21fdcb299a2a
gutao <gutaoai@126.com>
2015-12-29 23:21:01 +0800

    gutao

--- src/Exception/TestException.java
+++ src/Exception/TestException.java
@@ -1,7 +1,16 @@
 package Exception;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class TestException {
    public static void main(String[] args) {
 	System.out.println("测试异常类");
+	List list = new ArrayList();
+	if(list.isEmpty()){
+		list = new ArrayList();
+	}
+	System.out.println(list.toArray().toString());
 }
 }
