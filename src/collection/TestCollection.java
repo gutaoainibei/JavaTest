91b21d4179d52669ddae1b6f838dd3d58bdfab90
gutao <gutaoai@126.com>
2016-01-03 17:45:44 +0800

    Test Collection Desc

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 public class TestCollection {
     public static void main(String[] args) {
+    	//ArrayList:底层实现是数组:线程不安全，效率高，所以，查询快，修改插入删除慢
+    	//LinkedList:底层实现是链表：线程不安全，效率高，所以查询慢，修改插入删除快
+    	//Vector:线程安全，效率低
          List list = new ArrayList();
          list.add("aaa");
          list.add(new Date());
