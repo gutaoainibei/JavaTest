b35f69f4767b089827e57bd92c252edeb624f6b9
gt <gutaoai@126.com>
2016-01-17 11:51:24 +0800

    update mymapupdate 20160117-11:51

---
+++ src/MyMap/MyMapupdate1.java
@@ -0,0 +1,25 @@
+package MyMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import MyLinkedList.MyLinkedList;
+
+public class MyMapupdate1 {
+    private MyLinkedList[] entry = new MyLinkedList[10];
+    private int size;
+    public int size(){
+    	return this.size;
+    }
+    public void put(Object key,Object value){
+    	
+    }
+    public Object get(Object key){
+    	
+    	return null;
+    }
+	public static void main(String[] args) {
+         MyMapupdate1 myMap = new MyMapupdate1();
+	}
+
+}
