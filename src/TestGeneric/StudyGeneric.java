f04a5c182415830ef77e27fcaddfe3d8045fe085
gt <gutaoai@126.com>
2016-02-21 16:11:21 +0800

    testgeneric explain the generic

---
+++ src/TestGeneric/StudyGeneric.java
@@ -0,0 +1,51 @@
+package TestGeneric;
+/**
+ * 
+ * 描述：测试泛型类的继承
+ * @author gt
+ * @created 2016年2月21日 下午4:09:11
+ * @since
+ */
+
+public abstract class StudyGeneric <T1,T2>{
+     T1 age;
+     public abstract void test(T2 t2);
+}
+//不保留，这就是泛型的擦除
+//擦除之后，类型类似于Object
+class StudyGenericChild1<T1,T2> extends StudyGeneric{
+
+	@Override
+	public void test(Object t2) {
+		
+	}
+	
+}
+//部分保留
+class StudyGenericChild3<T1,Integer> extends StudyGeneric <T1,Integer>{
+
+	@Override
+	public void test(Integer t2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+class StudyGenericChild6<T1,Integer> extends StudyGeneric <T1,String>{
+
+	@Override
+	public void test(String t2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+//全不保留
+class StudyGenericChild4<String,Integer> extends StudyGeneric<String, Integer>{
+
+	@Override
+	public void test(Integer t2) {
+	
+	}
+	
+}
