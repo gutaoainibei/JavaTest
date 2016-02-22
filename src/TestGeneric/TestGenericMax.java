89fa9bbe0a210cb119fbf590f464a2edf834d8a4
gt <gutaoai@126.com>
2016-02-23 00:19:31 +0800

    Test range of generic class

--- src/TestGeneric/TestGenericMax.java
+++ src/TestGeneric/TestGenericMax.java
@@ -10,6 +10,9 @@ import java.util.List;
  * @author gt
  * @created 2016年2月22日 下午11:08:00
  * @since
+ * extends：泛型的上限<= ，即子类
+ * 1.一般用于限制操作
+ * 2.不能使用在添加数据上面，一般都是读取操作
  */
 public class TestGenericMax {
 	public static void main(String[] args) {
@@ -23,7 +26,16 @@ public class TestGenericMax {
     		   
 	}
 	public static void test(List<? extends Fruit> list){
-		
+	   //这里不能往list里面添加元素，因为我们继承Fruit我们无法确定list里面的元素是那个类型
+	   //前一个类型，是不是要比后一个类型范围更大，所以编译根本无法确定
+	   //The method add(capture#1-of ? extends Fruit) in the type List<capture#1-of ? extends Fruit> 
+	   // is not applicable for the arguments (Apple)
+	   //list.add(new Apple());	
+	}
+	public static void test1(List<? super Fruit> list){
+		//这里我们可以加比Fruit的子类，因为上限是确定了得，所以和上面相比，我们不存在编译的不确定性和错误性
+		list.add(new Fruit());
+		list.add(new Apple());
 	}
 }
 
