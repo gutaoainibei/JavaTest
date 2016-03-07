76f2e7f0c1bf3f7bc6aa66b6c22a1ce759957adf
gt <gutaoai@126.com>
2016-03-07 23:09:39 +0800

    add method mySort 2

--- src/TestSort/TestMyUtils.java
+++ src/TestSort/TestMyUtils.java
@@ -1,13 +1,42 @@
 package TestSort;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import TestSort.mySort.MySortUtil;
 
 public class TestMyUtils {
 	public static void main(String[] args) {
 		Integer[] arr = {7,5,9,8,46,4,34};
+		String[] str = {"acc","dcc","cdd","e"};
+		Date[] dates = new Date[3];
+		dates[0] = new Date();
+		dates[1] = new Date(System.currentTimeMillis()-1000*60*60);
+		dates[2] = new Date(System.currentTimeMillis()+1000*60*60);
+		
 		MySortUtil.Sort(arr);
+		MySortUtil.Sort(str);
+		MySortUtil.Sort(dates);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		for (int i = 0; i < str.length; i++) {
+			System.out.print(str[i]+" ");
+		}
+		System.out.println();
+		for (int i = 0; i < dates.length; i++) {
+			System.out.println(dates[i]);
+		}
+		List<String> list1 = new ArrayList<String>();
+		list1.add("adsdf");
+		list1.add("dsd");
+		list1.add("csss");
+		list1.add("bsda");
+		MySortUtil.Sort(list1);
+		for (int i = 0; i < list1.size(); i++) {
+			System.out.println(list1.get(i));
 		}
 	}
 }
--- src/TestSort/mySort/MySortUtil.java
+++ src/TestSort/mySort/MySortUtil.java
@@ -1,13 +1,30 @@
 package TestSort.mySort;
 
-public class MySortUtil{
+import java.util.List;
 
+public class MySortUtil{
+	public static  <T> void Sort(List<T> list){
+		Object[] t = list.toArray();
+		Sort(t);
+		for (int i = 0; i < t.length; i++) {
+			list.set(i, (T) t[i]);
+		}
+	}
+	public static <T extends Comparable<T>> void Sort(T[] arr){
+		   int count = 0;
+		   for (int i = 0; i < arr.length-1; i++) {
+			 for (int j = 0; j < arr.length-i-1; j++) {
+				if(((Comparable)arr[j]).compareTo(arr[j+1])<0){
+					T object = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = object;
+				}
+			}
+		}
+   }	
    public static void Sort(Object[] arr){
 	   int count = 0;
-	   int len = arr.length;
-	   System.out.println(arr.length);
 	   for (int i = 0; i < arr.length-1; i++) {
-		   System.out.println(i);
 		 for (int j = 0; j < arr.length-i-1; j++) {
 			if(((Comparable)arr[j]).compareTo(arr[j+1])<0){
 				Object object = arr[j];
