41843fdfd3cd24fe0ab962fc7adeb399579ff564
gt <gutaoai@126.com>
2016-03-30 21:42:01 +0800

    test dataStream

---
+++ src/TestIO/dataStream/TestDataStream.java
@@ -0,0 +1,17 @@
+package TestIO.dataStream;
+/**
+ * 描述：处理流
+ *     输出流：DataInputStream 
+ *     输出流：DataOutputStream
+ * @author gt
+ * @created 2016年3月30日 下午6:07:04
+ * @since
+ */
+public class TestDataStream {
+    public static void main(String[] args) {
+		
+	}
+    public static void write(String strPath){
+    	
+    }
+}
