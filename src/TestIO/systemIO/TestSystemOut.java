fd498297f83ea00aafb682acb4dc7b3b738af593
gt <gutaoai@126.com>
2016-04-03 00:36:28 +0800

    test update systemio

--- src/TestIO/systemIO/TestSystemOut.java
+++ src/TestIO/systemIO/TestSystemOut.java
@@ -1,13 +1,31 @@
 package TestIO.systemIO;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+import TestIO.FileUtils;
+
 /**
  * 
- * 描述：
+ * 描述：打印流，处理流
  * @author gt
  * @created 2016年4月2日 下午11:55:59
  * @since
  */
 public class TestSystemOut {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("test");
+		PrintStream ps = System.out;
+		ps.println(false);
 		
+		File file = new File("F:/JavaIO/systemio.txt");
+		//ps = new PrintStream(new BufferedOutputStream(new FileOutputStream(file)));
+		ps = new PrintStream(file);//这里打印流是输出流，这里指定要写出到file文件中
+		ps.println("gutao ai ni");
+		ps.flush();
+		ps.close();
 	}
 }
