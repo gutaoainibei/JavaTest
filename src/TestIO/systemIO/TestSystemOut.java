0261d32f3c91c09dff2b7c4630b712fd84249140
gt <gutaoai@126.com>
2016-04-02 23:56:58 +0800

    Test systemio

---
+++ src/TestIO/systemIO/TestSystemOut.java
@@ -0,0 +1,13 @@
+package TestIO.systemIO;
+/**
+ * 
+ * 描述：
+ * @author gt
+ * @created 2016年4月2日 下午11:55:59
+ * @since
+ */
+public class TestSystemOut {
+    public static void main(String[] args) {
+		
+	}
+}
