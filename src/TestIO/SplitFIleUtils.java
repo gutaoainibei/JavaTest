2a5a54fcd17b83f1dda159fef76ad684e27fff07
gt <gutaoai@126.com>
2016-04-10 01:24:15 +0800

    update SplitFileUtils and add method mergeBlock

--- src/TestIO/SplitFIleUtils.java
+++ src/TestIO/SplitFIleUtils.java
@@ -1,14 +1,22 @@
  package TestIO;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.RandomAccessFile;
+import java.io.SequenceInputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
+
+import org.apache.ibatis.jdbc.SqlBuilder;
 
 /**
  * 
@@ -20,6 +28,8 @@ import java.util.List;
 public class SplitFIleUtils {
 	 //要分割的文件路径
      private String filePath;
+     //存储的目标路径
+     private String destPath;
      //文件名称
      private String fileName;
      //文件的总字节数
@@ -33,12 +43,13 @@ public class SplitFIleUtils {
      public SplitFIleUtils(){
     	 blockPathList = new ArrayList<String>();
      }
-     public SplitFIleUtils(String filePath){
-    	 this(filePath,1024L);
+     public SplitFIleUtils(String filePath,String destPath){
+    	 this(filePath,destPath,1024L);
      }
-     public SplitFIleUtils(String filePath,long blockSize){
+     public SplitFIleUtils(String filePath,String destPath,long blockSize){
     	 this();
     	 this.filePath = filePath;
+    	 this.destPath = destPath;
     	 this.blockSize = blockSize;
     	 init();
      }
@@ -75,7 +86,7 @@ public class SplitFIleUtils {
       * @param destPath
      * @throws IOException 
       */
-     public void Split(String destPath) throws IOException{
+     public void Split() throws IOException{
     	 File fileDirectory = new File(destPath);
     	 if(!(fileDirectory.isDirectory())){
     		 System.out.println("目标目录不存在");
@@ -124,8 +135,58 @@ public class SplitFIleUtils {
  		   }
            FileUtils.Close(randomAccessFile,bufferedStream);
      }
+     /**
+      * 
+      * 描述：实现文件合并
+      * @author gt
+      * @created 2016年4月10日 上午12:56:34
+      * @since 
+      * @param mergePath 合并的地址
+      * @throws IOException
+      */
+     public void mergeBolck(String mergePath) throws IOException{
+    	 File file = new File(mergePath);
+    	 OutputStream OutputStream = new BufferedOutputStream(new FileOutputStream(file,true));
+    	 InputStream inputStream = null;
+    	 for (int i = 0; i < blockPathList.size(); i++) {
+    		 inputStream = new BufferedInputStream(new FileInputStream(new File(blockPathList.get(i))));
+		     byte[] flush = new byte[1024];
+		     int len = 0;
+		     while (-1!=(len = inputStream.read(flush))) {
+                  OutputStream.write(flush, 0, len);
+			 }
+		     inputStream.close();
+    	 }
+    	 FileUtils.Close(OutputStream);
+     }
+     /**
+      * 
+      * 描述：通过SequenceInputStream来先把所有输入流汇成一个输入流
+      * @author gt
+      * @created 2016年4月10日 上午1:17:15
+      * @since 
+      * @param mergePath
+      * @throws IOException
+      */
+     public void mergeBlockSequenceInputStream(String mergePath) throws IOException{
+    	 OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(new File(mergePath),true));
+    	 Vector<InputStream> vector = new Vector<InputStream>();
+    	 InputStream inputStream = null;
+    	 for (int i = 0; i < blockPathList.size(); i++) {
+    		 inputStream = new BufferedInputStream(new FileInputStream(new File(blockPathList.get(i))));
+    	     vector.add(inputStream);
+    	 }
+    	 SequenceInputStream sequenceInputStream = new SequenceInputStream(vector.elements());
+    	 byte[] flush = new byte[1024];
+    	 int len = 0;
+    	 while (-1!=(len = sequenceInputStream.read(flush))) {
+			  outputStream.write(flush, 0, len);
+		}
+    	FileUtils.Close(sequenceInputStream,outputStream);
+     }
      public static void main(String[] args) throws IOException {
-		SplitFIleUtils splitFIleUtils = new SplitFIleUtils("F:/JavaIO/lm/1.jpg", 5000);
-	    splitFIleUtils.Split("F:/JavaIO/lm");
+		SplitFIleUtils splitFIleUtils = new SplitFIleUtils("F:/JavaIO/lm/大四文档.docx","F:/JavaIO/lm", 5000);
+	    splitFIleUtils.Split();
+	    splitFIleUtils.mergeBlockSequenceInputStream("F:/JavaIO/lm/合成文档.docx");
      }
 }
