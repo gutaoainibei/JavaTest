6e880d889136f8fb7e7b533bb38d3e2bc63fb426
gt <gutaoai@126.com>
2016-04-28 18:38:40 +0800

    update myServer 1.0

--- src/Socket/server/MyServer.java
+++ src/Socket/server/MyServer.java
@@ -1,21 +1,122 @@
 package Socket.server;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
 
 /**
  * 
  * 描述：实现自己简单服务器
+ *    协议
+ *    1）应用层HTTP FTP TELNET SNMP DNS
+ *    2)传输层 TCP UDP
+ *    3)网路层IP
+ *    4）主机-网络层  以太网 IEEE802.3 令牌环网：IEEE802.4
+ *    HTTP（Hypertext Transfer Protocol）是网络应用层的协议，建立
+ *    在TCP/IP协议基础上，HTTP协议使用可靠的TCP连接，默认端口为80，目前
+ *    最新 的版本为HTTP2.0，对应的RFC文档为RFC2068
+ *    
+ *    Http协议请求格式
+ *    1）请求方式  URL（统一资源定位符）  Http协议/版本
+ *    2）请求头（Request Header）
+ *    3）请求正文（Request Content）
+ *          例如：
+ *          POST /index.html HTTP/1.1
+			Host: localhost:8888
+			Connection: keep-alive
+			Content-Length: 35（这里是字节数）
+			Cache-Control: max-age=0
+			Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp;q=0.8
+			Origin: null
+			Upgrade-Insecure-Requests: 1
+			User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36
+			Content-Type: application/x-www-form-urlencoded
+			Accept-Encoding: gzip, deflate
+			Accept-Language: zh-CN,zh;q=0.8
+
+            username=%B9%CB%CC%CE&password=1111
+ *    Http协议响应格式
+ *    1）Http协议版本 状态码 描述（两两之间有一个空格）
+ *    2）响应头(Response Head)
+ *    3)响应正文(Response Content)
+ *   
+ *    常见类型:
+ *    
+ *    
+ *    头字段                                    定义
+      Accept               客户端可以处理的媒体类型（MIME-Type），按优先级排序；在一个以逗号为分隔的列表中，可以定义多种类型和使用通配符
+      Accept-Language      客户端支持的自然语言列表
+      Accept-Encoding      客户端支持的编码列表
+      User-Agent           客户端环境类型
+      Host                 服务器端的主机地址
+      Connection           连接类型，默认为Keep-Alive
+ *    
+ *    状态码                                    定义
+       1xx                 报告接收到请求，继续进程
+       2xx                 成功步骤成功接收，被理解，并被接受
+       3xx                 重定向为了完成请求,必须采取进一步措施
+       4xx                 客户端出错请求包括错的顺序或不能完成
+       5xx                 服务器出错服务器无法完成显然有效的请求
+       下面列举了为HTTP/1.1定义的状态码值，和对应的原因短语（Reason-Phrase）的例子。
+		l  客户端错误
+		"100" : Continue  继续
+		"101" : witching Protocols  交换协议
+		l  成功
+		"200" : OK
+		"201" : Created 已创建
+		"202" : Accepted 接收
+		"203" : Non-Authoritative Information 非认证信息
+		"204" : No Content 无内容
+		"205" : Reset Content 重置内容
+		"206" : Partial Content 部分内容
+		l  重定向
+		"300" : Multiple Choices 多路选择
+		"301" : Moved Permanently  永久转移
+		"302" : Found 暂时转移
+		"303" : See Other 参见其它
+		"304" : Not Modified 未修改
+		"305" : Use Proxy 使用代理
+		"307" : Temporary Redirect
+		l  客户方错误
+		"400" : Bad Request 错误请求
+		"401" : Unauthorized 未认证
+		"402" : Payment Required 需要付费
+		"403" : Forbidden 禁止
+		"404" : Not Found 未找到
+		"405" : Method Not Allowed 方法不允许
+		"406" : Not Acceptable 不接受
+		"407" : Proxy Authentication Required 需要代理认证
+		"408" : Request Time-out 请求超时
+		"409" : Conflict 冲突
+		"410" : Gone 失败
+		"411" : Length Required 需要长度
+		"412" : Precondition Failed 条件失败
+		"413" : Request Entity Too Large 请求实体太大
+		"414" : Request-URI Too Large 请求URI太长
+		"415" : Unsupported Media Type 不支持媒体类型
+		"416" : Requested range not satisfiable
+		"417" : Expectation Failed
+		l  服务器错误
+		"500" : Internal Server Error 服务器内部错误
+		"501" : Not Implemented 未实现
+		"502" : Bad Gateway 网关失败
+		"503" : Service Unavailable
+		"504" : Gateway Time-out 网关超时
+		"505" : HTTP Version not supported  HTTP版本不支持
+ *    
  * @author gt
  * @created 2016年4月24日 下午10:58:37
  * @since
  */
 public class MyServer {
     private ServerSocket server;
+    private static final String CRLF="\r\n";
+    private static final String BLANK=" ";
     public static void main(String[] args) {
 		MyServer serverTest = new MyServer();
 		serverTest.start();
@@ -63,6 +164,25 @@ public class MyServer {
 			int len = inputStream.read(flush);
 		    System.out.println("获取到的请求信息：");
 		    System.out.println(new String(flush,0,len));
+		    
+		    StringBuffer responseContext = new StringBuffer();
+		    responseContext.append("<html><head><title>响应实例</title>"+
+		    "</head><body></body><table border='1'><tr><th>Month</th><th>顾涛</th></tr><tr><td>January</td><td>$100</td></tr></table>");
+    	    StringBuffer response = new StringBuffer();
+    	    //1.http协议版本 状态码  描述
+    	    response.append("HTTP/1.1").append(BLANK).append("200").append(BLANK).append("gutao success");
+    	    //2.头信息（Response Head）
+    	    response.append("Server:gt Server/1.0.0").append(CRLF);
+    	    response.append("Date:").append(new Date()).append(CRLF);
+    	    response.append("Content-type:text/html;charset=UTF-8").append(CRLF);
+    	    response.append("Content-Length").append(responseContext.toString().getBytes().length).append(CRLF);
+    	    response.append(CRLF);
+    	    response.append(responseContext);
+    	    
+		    BufferedOutputStream out = new BufferedOutputStream(simple_socket.getOutputStream());
+		    out.write(response.toString().getBytes());
+		    out.flush();
+		    out.close();
     	} catch (IOException e) {
 			e.printStackTrace();
 		}
