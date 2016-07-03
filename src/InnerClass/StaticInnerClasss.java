bb84426562108527c31721da35190df4f305b5bb
gt <gutaoai@126.com>
2016-07-03 23:11:16 +0800

    finish InnerClass test

--- src/InnerClass/StaticInnerClasss.java
+++ src/InnerClass/StaticInnerClasss.java
@@ -1,4 +1,8 @@
 package InnerClass;
+
+import InnerClass.StaticInnerClasss.gt;
+import InnerClass.StaticInnerClasss.test1;
+
 /**
  * 
  * 描述：静态内部类测试
@@ -19,6 +23,54 @@ public class StaticInnerClasss {
     	}
     }
     public static void test(){
+    	class gtainibei{
     	
+    	}
     }
+    public void gutao(){
+    	int ag;
+    	//方法内部类不能定义静态成员属性
+    	//static String name;
+    	class nibei{
+    		int gg;
+    		void gutaoainibei(){
+    			//方法内部类只在方法中有效，而且不能访问方法的成员变量，
+    			//只能访问final修饰的编译时可以确定的常量表达式，new Date()就无法识别
+    			//System.out.println(ag);
+    		}
+    		Test test = new Test() {
+				//方法匿名内部类
+				@Override
+				public void run() {
+					
+				}
+			};
+    	}
+    }
+    //成员内部类，这个是个对象绑定的
+    public class gt{
+    	void test(){
+    		
+    	}
+    }
+    //类名内部类
+    Test test = new Test() {
+		
+		@Override
+		public void run() {
+			System.out.println(name);
+			System.out.println(age);
+		}
+	};
+}
+interface Test{
+	public void run();
+}
+class Demo{
+	public static void main(String[] args) {
+		//静态内部类可以直接访问
+		test1 t = new test1();
+        //成员内部类是绑定在对象上的
+		gt g = new StaticInnerClasss().new gt();
+	}
 }
