581fce1553479a20eefca00a711cdd95061bf659
gt <gutaoai@126.com>
2016-07-21 23:33:41 +0800

    test design of facade

---
+++ src/designPattern/facade/Client.java
@@ -0,0 +1,11 @@
+package designPattern.facade;
+/**
+ * 
+ * 描述：测试外观模式
+ * @author gt
+ * @created 2016年7月21日 下午11:32:30
+ * @since
+ */
+public class Client {
+
+}
---
+++ src/designPattern/facade/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 描述：
+ * @author gt
+ * @created 2016年7月21日 下午11:32:17
+ * @since 
+ */
+package designPattern.facade;
