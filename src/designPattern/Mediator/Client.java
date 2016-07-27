6f25d8d28298a85009a68b666543f0ff545a5f75
gt <gutaoai@126.com>
2016-07-27 23:36:57 +0800

    test design of mediator

---
+++ src/designPattern/Mediator/Client.java
@@ -0,0 +1,11 @@
+package designPattern.Mediator;
+/**
+ * 
+ * 描述：测试中介者模式
+ * @author gt
+ * @created 2016年7月27日 下午11:34:52
+ * @since
+ */
+public class Client {
+
+}
---
+++ src/designPattern/Mediator/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 描述：
+ * @author gt
+ * @created 2016年7月27日 下午11:34:34
+ * @since 
+ */
+package designPattern.Mediator;
