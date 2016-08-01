25dd28d41abb69605a82ff300eb03b02514500b7
gt <gutaoai@126.com>
2016-08-01 23:08:00 +0800

    begin studying regular

---
+++ src/regular/Client.java
@@ -0,0 +1,11 @@
+package regular;
+/**
+ * 
+ * 描述：测试正则表达式
+ * @author gt
+ * @created 2016年8月1日 下午11:06:25
+ * @since
+ */
+public class Client {
+
+}
---
+++ src/regular/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 描述：
+ * @author gt
+ * @created 2016年8月1日 下午11:05:52
+ * @since 
+ */
+package regular;
