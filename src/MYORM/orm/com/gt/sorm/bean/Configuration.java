315437c1b2f97427afabf184c4bd6d3093087b3f
gt <gutaoai@126.com>
2016-08-31 23:33:26 +0800

    update my Configuration

--- src/MYORM/orm/com/gt/sorm/bean/Configuration.java
+++ src/MYORM/orm/com/gt/sorm/bean/Configuration.java
@@ -1,5 +1,86 @@
 package MYORM.orm.com.gt.sorm.bean;
-
+/**
+ * 
+ * 描述：配置信息
+ * @author gt
+ * @created 2016年8月31日 下午11:17:11
+ * @since
+ */
 public class Configuration {
+	 /**
+	  * 驱动类
+	  */
+     private String driver;
+     /**
+      * jdbcURL
+      */
+     private String url;
+     /**
+      * 用户名
+      */
+     private String user;
+     /**
+      * 密码
+      */
+     private String password;
+     /**
+      * 所用数据库
+      */
+     private String usingDB;
+     /**
+      * java持久化类的地址
+      */
+     private String srcPath;
+     
+	public Configuration() {
+	}
+	
+	public Configuration(String driver, String url, String user,
+			String password, String usingDB, String srcPath) {
+		super();
+		this.driver = driver;
+		this.url = url;
+		this.user = user;
+		this.password = password;
+		this.usingDB = usingDB;
+		this.srcPath = srcPath;
+	}
 
+	public String getDriver() {
+		return driver;
+	}
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUsingDB() {
+		return usingDB;
+	}
+	public void setUsingDB(String usingDB) {
+		this.usingDB = usingDB;
+	}
+	public String getSrcPath() {
+		return srcPath;
+	}
+	public void setSrcPath(String srcPath) {
+		this.srcPath = srcPath;
+	}
+     
 }
