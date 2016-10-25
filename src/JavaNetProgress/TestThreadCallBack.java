ec7ad206698837441a78fc4ea8360ecf0444319f
gutao <gutaoai@126.com>
2016-10-26 00:04:00 +0800

    complete my callback test

--- src/JavaNetProgress/TestThreadCallBack.java
+++ src/JavaNetProgress/TestThreadCallBack.java
@@ -1,4 +1,17 @@
 package JavaNetProgress;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.xml.bind.DatatypeConverter;
+
+import oracle.net.aso.i;
+
 /**
  * 
 * @类名: TestThreadCallBack 
@@ -8,21 +21,64 @@ package JavaNetProgress;
 *
  */
 public class TestThreadCallBack {
-    public static void main(String[] args) {
-		for (String string : args) {
-			System.out.println(string);
-		}
-	}
-}
-abstract class callBackThread implements Runnable{
+	
 	private String fileName;
-	public callBackThread(){}
-	public callBackThread(String fileName) {
-		super();
+	private byte[] digest;
+    public TestThreadCallBack(String fileName) {
 		this.fileName = fileName;
 	}
-    public abstract <T> T call();
+    public void callThread(){
+    	callBackThread call = new callBackThread(fileName, this);
+    	new Thread(call).start();
+    }
+	public static void main(String[] args) {
+    	for(int i = 1 ; i < 3 ;i++){
+    		TestThreadCallBack t1 = new TestThreadCallBack("H:/test/JAVA.txt");
+    		t1.callThread();
+    	}
+	}
+
+	public void receiveDigest(byte[] digest) {
+		this.digest = digest;
+		System.out.println(this);
+	}
+	@Override
+	public String toString() {
+		String result = fileName +":";
+		if(digest != null){
+			result += DatatypeConverter.printHexBinary(digest);
+		}else{
+           result += "digest not avaliable";			
+		}
+		return result;
+	}
+	
+}
+class callBackThread implements Runnable{
+	private String fileName;
+	private TestThreadCallBack callbackObj;
+	public callBackThread(){}
+	public callBackThread(String fileName, TestThreadCallBack callbackObj) {
+		super();
+		this.fileName = fileName;
+		this.callbackObj = callbackObj;
+	}
+	
 	public void run() {
-		
+		try {
+			FileInputStream in = new FileInputStream(fileName);//"H:/test/JAVA.txt"
+			MessageDigest sha = MessageDigest.getInstance("SHA-256");
+			DigestInputStream digestInputStream = new DigestInputStream(in,sha); 
+			while (digestInputStream.read() != -1) ;
+			digestInputStream.close();
+			byte[] digest = sha.digest();
+			callbackObj.receiveDigest(digest);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
